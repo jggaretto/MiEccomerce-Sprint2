06d745876eaa326be34150a5ff0fa6f41f642642
Juan Cruz <jggaretto@gmail.com>
2026-04-21 15:39:38 -0300

    Cambio en productControllers , se agrega funcion getProductDetail

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -6,4 +6,15 @@ function getIndex(req, res) {
   res.render('pages/index', { title: 'Inicio', suggestedProducts, featuredProducts });
 }
 
-module.exports = { getIndex };
+function getProductDetail(req, res) {
+  const product = getProductById(req.params.id);
+
+  if (!product) {
+    return res.status(404).render('pages/error404', { title: 'Producto no encontrado' });
+  }
+
+  const related = getSuggestedProducts(4, true).filter((p) => p.id !== product.id).slice(0, 3);
+
+  res.render('pages/product', { title: product.name, product, related });
+}
+module.exports = { getIndex, getProductDetail };
